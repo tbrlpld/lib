ea84bd0fe1142184e80ec86ffacfc808c3d9868b
Tibor Leupold <tibor@lpld.io>
2025-08-07 16:13:25 -0700

    Add use-is-on-screen hook

---
+++ js/react/hooks/use-is-on-screen.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+
+/**
+ * Check is an element is on screen.
+ *
+ * Returns a boolean expressing the visibility and the ref to put on the element in question.
+ */
+function useIsOnscreen() {
+  const elementRef = React.useRef();
+  const [isOnScreen, setIsOnScreen] = React.useState(false);
+
+  React.useEffect(
+    () => {
+      if (!elementRef.current) return
+
+      const observer = new IntersectionObserver((entries) => {
+        const [entry] = entries;
+
+        // `entry.isIntersecting` will be true if the
+        // element is in the viewport, false if not.
+        setIsOnScreen(entry.isIntersecting)
+      });
+
+      observer.observe(elementRef.current);
+
+      return () => {
+        observer.disconnect();
+      }
+    }
+  )
+
+  return [isOnScreen, elementRef];
+}
+
+export default useIsOnscreen;
