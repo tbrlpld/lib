4dd297295f5d63980bb3c7a9ba0db1ceedd87684
Tibor Leupold <tibor@lpld.io>
2025-08-11 12:51:16 -0700

    Memoize the toggle function
    The function never changes. Thus, we don't need to recreate it on every
    re-render of the caller.
    
    Recreating this function could have performance implications when it is
    passed to large components that trigger expensive operations when
    rendering or have many descendants.

--- js/react/hooks/use-toggle.js
+++ js/react/hooks/use-toggle.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 function useToggle(initial = false) {
   if (typeof initial !== "boolean") {
-    throw new Error("useToggle can only be used with boolean initial values")
+    throw new Error("useToggle can only be used with boolean initial values.")
   }
 
   const [value, setValue] = React.useState(initial)
 
-  function toggle() {
+  const toggle = React.useCallback(() => {
     setValue(currentValue => !currentValue)
-  }
+  })
 
   return [value, toggle]
 }
