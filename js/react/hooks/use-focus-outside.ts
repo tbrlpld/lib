c6b3060771212d49698c1822945f2d687f1f7d81
Tibor Leupold <tibor@lpld.io>
2025-09-15 21:28:49 -0700

    Fix types of focus outside

--- js/react/hooks/use-focus-outside.ts
+++ js/react/hooks/use-focus-outside.ts
@@ -2,30 +2,35 @@ import * as React from 'react'
 
 
 /**
- * Hook to run callback when focus is outside of the container element.
+ * Hook to run callback when focus is outside the container element.
  *
  * Pass the callback to run when the focus moves outside.
  *
  * Add the returned ref and blur handler to the container element of interest.
  */
-export default function useFocusOutside (callback: () => void) {
-  const focusOutsideContainerRef = React.useRef<HTMLElement>(null)
+export default function useFocusOutside<Type extends HTMLElement> (
+  callback: () => void,
+): {
+  ref: React.RefObject<Type | null>,
+  handleBlur: (event: React.FocusEvent) => void,
+} {
+  const ref = React.useRef<Type | null>(null)
 
 
   function handleBlur (event: React.FocusEvent) {
-    if (!focusOutsideContainerRef.current || !event.relatedTarget) {return}
+    if (!ref.current || !event.relatedTarget) {return}
 
-    const formEl = focusOutsideContainerRef.current as HTMLFormElement
+    const containerElement = ref.current as Type
     const newlyFocusedElement = event.relatedTarget as HTMLElement
 
-    if (!formEl.contains(newlyFocusedElement)) {
+    if (!containerElement.contains(newlyFocusedElement)) {
       callback()
     }
   }
 
 
   return {
-    focusOutsideContainerRef: focusOutsideContainerRef,
+    ref,
     handleBlur,
   }
 }
