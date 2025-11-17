04528a4c2e11574272fcc11e25765742a8ac798e
Tibor Leupold <tibor@lpld.io>
2025-11-16 20:24:08 -0800

    Add download module

---
+++ js/download.ts
@@ -0,0 +1,17 @@
+interface Props {
+  filename?: string,
+  content: string,
+  mimeType?: string,
+}
+
+
+export default ({
+  content,
+  filename = 'file.txt',
+  mimeType = 'text/plain',
+}: Props): void => {
+  const downloadLink = document.createElement('a')
+  downloadLink.href = `data:${ mimeType },${ encodeURIComponent(content) }`
+  downloadLink.download = filename
+  downloadLink.click()
+}
