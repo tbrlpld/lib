27ec98eaf8e4d6dd68f7a79a7b840c02d98371c5
Tibor Leupold <tibor@lpld.io>
2025-12-16 15:36:17 -0800

    Add missing import

--- py/django/url.py
+++ py/django/url.py
@@ -1,6 +1,8 @@
+import urllib.parse
+
 import django.utils.http
 
 def append_query_string(url: str, params: dict) -> str:
     """Append a query string based on the given dict of key-value pairs to the url."""
     query_string = django.utils.http.urlencode(params)
-    return urlparse.urljoin(base=url, url="?" + query_string)
+    return urllib.parse.urljoin(base=url, url="?" + query_string)
