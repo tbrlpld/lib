d6555213919721b4d6af52683a345b7acb95d548
Tibor Leupold <tibor@lpld.io>
2026-01-14 13:54:08 -0800

    Ensure page factory works with subfactories

--- py/django/lib/factories/page.py
+++ py/django/lib/factories/page.py
@@ -10,16 +10,18 @@ class PageFactory(factory.django.DjangoModelFactory):
     title = factory.Faker("words", nb=3)
 
     @classmethod
-    def create(cls, **kwargs) -> T:
+    def _create(cls, model_class, *args, **kwargs) -> T:
         try:
             parent = kwargs.pop("parent")
         except KeyError as error:
-            raise AttributeError("`parent` argument needs to be defined for page factory.") from error
+            raise AttributeError(
+                "`parent` argument needs to be defined for page factory."
+            ) from error
 
         if not isinstance(parent, Page):
             raise TypeError("`parent` needs to be a Page.")
 
-        instance = super().build(**kwargs)
+        instance = super()._build(model_class, *args, **kwargs)
 
         parent.add_child(instance=instance)
 
